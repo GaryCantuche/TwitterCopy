980e53c14da89d100aa2b4268a0243e6f989ff05
GaryCantuche <garycantuche2000@gmail.com>
2020-08-27 13:48:38 +0200

    Added alt to proifle banner image

--- frontend/src/Components/Profile/ImageBanner.js
+++ frontend/src/Components/Profile/ImageBanner.js
@@ -8,7 +8,7 @@ function ImageBanner (props) {
 
     return (
         <div onClick={onClick} className="profile__banner__img">
-            <img src={`data:image/png;base64,${props.image}`}/>
+            <img src={`data:image/png;base64,${props.image}`} alt="profile user banner"/>
         </div>
     );
 }
